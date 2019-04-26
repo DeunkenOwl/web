7bf797ee06040983a6769e8f1f73153c29ce62c7
Egor <kolchev-00@mail.ru>
2019-04-26 11:33:47 +1000

    some impruvement

--- task4/script4.2.js
+++ task4/script4.2.js
@@ -14,7 +14,7 @@ function fibArr(n) {
 }
 
 function fib1(form) {
-	var n = Number.parseInt(form.N.value)
+	var n = Number.parseInt(form.N.value);
 	if (n > 0) {
 		var arr = fibArr(n);
 		alert(n + "-й член последовательности фибоначчи:" + arr[n - 1]);
@@ -24,7 +24,7 @@ function fib1(form) {
 }
 
 function fib2(form) {
-	var n = Number.parseInt(form.N.value)
+	var n = Number.parseInt(form.N.value);
 	if (n > 0) {
 		var arr = fibArr(n);
 		var x = 0;
@@ -38,7 +38,7 @@ function fib2(form) {
 }
 
 function fib3(form) {
-	var n = Number.parseInt(form.N.value)
+	var n = Number.parseInt(form.N.value);
 	if (n > 0) {
 		var arr = fibArr(n);
 		var x = 0;
@@ -52,14 +52,81 @@ function fib3(form) {
 	}
 }
 
-function v1(form) {
-	var n = Number.parseInt(form.X.value)
+function calc1(form) {
+	var n = Number.parseInt(form.X.value);
 	if (n > 0) {
 		var x = 1;
 		for (var i = 1; i <= n; i++) {
 			x = x * (x ^ i + x + 1);
 		}
 		alert("Произведение: " + x);
+	} else {
+		alert("Неправильный ввод");
+	}
+}
+
+function calc2(form) {
+	var n = Number.parseInt(form.X.value);
+	if (n > 0) {
+		var x = 0;
+		for (var i = 1; i <= n; i++) {
+			x = x + n / (2 * i - 1);
+		}
+		alert("Произведение: " + x);
+	} else {
+		alert("Неправильный ввод");
+	}
+}
+
+function calc3(form) {
+	var a = 0;
+	var b = (1 + 10) / 2 * 10;
+	for (var i = 3; i <= 33; i += 3) {
+		a += i;
+	}
+	alert(a / b);
+}
+
+function calc4(form) {
+	var a = (1 + 40) / 2 * 40;
+	var b = 0;
+	for (var i = 5; i <= 70; i += 5) {
+		b += i;
+	}
+	alert(a * b);
+}
+
+function calc5(form) {
+	var x = Number.parseFloat(form.X.value);
+	if (x > 0 && x < 1) {
+		var out = 0;
+		var i = 0;
+		var V = x ^ 2 / (i * 4 + 2);
+		while (V > 0.0005) {
+			out += V;
+			i++;
+			V = x / (2 + i * 4);
+		}
+		alert(out);
+	} else {
+		alert("Неправильный ввод");
+	}
+}
+
+function calc6(form) {
+	var x = Number.parseInt(form.X.value);
+	var arr = new Array(16);
+	//alert(x);
+	if (x > 0) {
+		for (var i = 1; i < 16; i++) {
+			arr[i] = x ^ (1 + i * 4) / (4 * i);
+			//alert(arr[i]);
+		}
+		x = 0;
+		for (var i = 3; i < 16; i++) {
+			x += arr[i];
+		}
+		alert(x);
 	} else {
 		alert("Неправильный ввод");
 	}
