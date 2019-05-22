a13314719ed745d125f4b0080d121ad7ae6f51df
Egor <kolchev-00@mail.ru>
2019-05-22 18:36:36 +1000

    added task6

---
+++ task6/Task6/Task6/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Task6.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Self
+        public ActionResult Self()
+        {
+            return View();
+        }
+
+        // GET: Verse
+        public ActionResult Verse()
+        {
+            return View();
+        }
+    }
+}
